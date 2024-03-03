6afc0f5146d1d8d61d7ff0e97e83b8dda8d81d36
Farwa khan <160652079+FARWA-KHAN@users.noreply.github.com>
2024-03-03 21:10:14 +0500

    Add files via upload

---
+++ 11.Greetings.ts
@@ -0,0 +1,8 @@
+let names :string[] = ['farwa','anosha','fardeen','waseem'];
+
+const message :string = 'Welcome to Typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+}
